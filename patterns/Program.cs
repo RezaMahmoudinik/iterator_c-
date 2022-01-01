4ed4029806bf88de2dc37f22336cfadfc939bbb6
reza <rezamahmoudinik1380@gmail.com>
2022-01-01 16:42:56 +0330

    commiting from pc

---
+++ patterns/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteAgregate a = new ConcreteAgregate();
+            a[0]=  "item a";
+            a[1] = "item b";
+            a[2] = "item c";
+            a[3] = "item d";
+            Iterator b = a.create();
+            Console.WriteLine("over collection:");
+            object item = b.first();
+            while(item!=null)
+            {
+                Console.WriteLine(item);
+                item = b.next();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ patterns/classTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patterns
+{
+    abstract class Agregate
+    {
+        public abstract Iterator create();
+    }
+    class ConcreteAgregate : Agregate
+    {
+        private ArrayList item = new ArrayList();
+        public override Iterator create()
+        {
+            return new ConcreteIterator(this);
+        }
+        public int counte
+        {
+            get { return item.Count; }
+        }
+        public object this[int index]
+        {
+            get { return item.Count; }
+            set    { item.Insert(index, value); }
+        }
+    }
+    abstract class Iterator
+    {
+        public abstract Object first();
+        public abstract Object next();
+        public abstract bool done();
+
+        public abstract object items();
+
+
+
+    }
+    class ConcreteIterator : Iterator
+    {
+        private ConcreteAgregate agg;
+        private int curen = 0;
+        public ConcreteIterator(ConcreteAgregate aggregate)
+        {
+            this.agg = aggregate;
+        }
+        public override Object first()
+        {
+            return agg[0];
+        }
+        public override object next()
+        {
+            object ret = null;
+            if (curen < agg.counte - 1)
+            {
+                ret = agg[++curen];
+            }
+            return ret;
+        }
+        public override bool done()
+        {
+            return curen >= agg.counte;
+        }
+
+        public override object items()
+        {
+            return agg[curen];
+
+
+        } 
+    
+
+    }
+
+}
